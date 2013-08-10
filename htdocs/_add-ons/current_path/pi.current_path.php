02ec9d344ace79b4b164122433006aa21a30d314
Nob Y <nob@nnusunn.jp>
2013-08-11 01:53:36 +0900

    current_path already exists in $app->config

--- htdocs/_add-ons/current_path/pi.current_path.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Plugin_current_path extends Plugin
-{
-    public function index()
-    {
-    	// return Path::tidy(Config::getSiteRoot() . URL::getCurrent());
-    	return Path::tidy(URL::getCurrent());
-    }
-}
